277f936f66545b01ee9f1a9a69376f62d3f530f7
Mitchell Levy <levymitchell0@gmail.com>
2021-02-08 16:09:57 -0800

    Add the ability to set guild-specific prefixes

--- src/index.ts
+++ src/index.ts
@@ -18,11 +18,11 @@ interface GuildData {
     calendars: Calendar[],
 }
 interface SaveData {
-    guilds: GuildData[],
+    guilds: [string, GuildData][],
 }
 
 // Globals
-let guilds: GuildData[];
+let guilds: Map<string, GuildData>;
 const discordClient: DiscordClient = new DiscordClient();
 const guildIdToPrefix: Map<string, string> = new Map<string, string>();
 const throwExpr = (msg: any): never => { // Hack because `throw` isn't an expression
@@ -31,66 +31,109 @@ const throwExpr = (msg: any): never => { // Hack because `throw` isn't an expres
 const SAVE_FILE: string = process.env.SAVE_FILE ?? './data.json';
 const DEFAULT_PREFIX: string = process.env.DEFAULT_PREFIX ?? '~';
 const DISCORD_SECRET: string = process.env.DISCORD_SECRET ?? throwExpr('A DISCORD_SECRET must be provided in `./.env`!');
-const EMPTY_SAVE_DATA: string = JSON.stringify({ guilds: [] });
+// Makes sure that the empty save data gets updated if the SaveData type is changed
+const EMPTY_SAVE_DATA_VALUE: SaveData = { guilds: [] };
+const EMPTY_SAVE_DATA: string = JSON.stringify(EMPTY_SAVE_DATA_VALUE);
 
 async function saveData(): Promise<void> {
-    const output: SaveData = { guilds };
+    const output: SaveData = { guilds: Array.from(guilds.entries()) };
     return fs.writeFile(SAVE_FILE, JSON.stringify(output));
 }
 
 async function startUp(): Promise<void> {
+    const login: Promise<string> = discordClient.login(DISCORD_SECRET);
+
     let file: string;
     try {
         file = await fs.readFile(SAVE_FILE, { encoding: 'utf-8' });
     } catch (err) {
         file = EMPTY_SAVE_DATA;
         if (err.code === 'ENOENT') {
-            fs.writeFile(SAVE_FILE, EMPTY_SAVE_DATA, 'utf-8');
+            await fs.writeFile(SAVE_FILE, EMPTY_SAVE_DATA, 'utf-8');
         } else {
             throw err;
         }
     }
     const savedData: SaveData = JSON.parse(file);
-
-    guilds = savedData.guilds;
-    savedData.guilds.forEach((guild: GuildData) => {
+    guilds = new Map(savedData.guilds);
+    guilds.forEach((guild: GuildData) => {
         guildIdToPrefix.set(guild.id, guild.prefix);
     });
 
-    await discordClient.login(DISCORD_SECRET);
+    await login;
     discordClient.guilds.cache.forEach((guild: Guild, id: string) => {
         if (!guildIdToPrefix.has(id)) {
             guildIdToPrefix.set(id, DEFAULT_PREFIX);
-            guilds.push({ id, prefix: DEFAULT_PREFIX, calendars: [] });
+            guilds.set(id, { id, prefix: DEFAULT_PREFIX, calendars: [] });
         }
     });
     saveData();
+}
 
+function handleCommand(commandTokens: string[], msg: Message): void {
+    switch (commandTokens[0].toLowerCase()) {
+        case 'ping':
+            msg.reply('pong!');
+            break;
+        case 'prefix':
+            if (msg.guild !== null) {
+                if (commandTokens.length === 1) {
+                    msg.reply(`the current prefix is \`${guildIdToPrefix.get(msg.guild.id)}\``);
+                } else if (commandTokens.length === 2) {
+                    msg.reply(`the current prefix is \`${guildIdToPrefix.get(msg.guild.id)}\`. Did you mean \`${guildIdToPrefix.get(msg.guild.id)}setprefix\`?`);
+                } else {
+                    msg.reply(`Invalid usage of \`${guildIdToPrefix.get(msg.guild.id)}prefix\``);
+                }
+            } else {
+                msg.reply('This command can only be used within a server.');
+            }
+            break;
+        case 'setprefix':
+            if (msg.guild !== null) {
+                if (commandTokens.length === 2) {
+                    const newPrefix = commandTokens[1];
+                    const msgGuildData: GuildData | undefined = guilds.get((msg.guild as Guild).id);
+                    if (msgGuildData !== undefined) {
+                        msgGuildData.prefix = newPrefix;
+                        guildIdToPrefix.set(msgGuildData.id, newPrefix);
+                        saveData();
+                        msg.reply(`done! New prefix is \`${newPrefix}\`.`);
+                    }
+                } else {
+                    msg.reply('This command needs the new prefix to use.');
+                }
+            } else {
+                msg.reply('This command can only be used within a server.');
+            }
+            break;
+        default:
+            msg.reply('invalid command!');
+    }
 }
 
 async function setUpListeners(): Promise<void> {
     discordClient.on('message', (msg: Message) => {
         const prefix: string = msg.guild === null ? '' : guildIdToPrefix.get(msg.guild.id) as string;
-
-        if (msg.content.startsWith(prefix)) {
+        const startsWithPrefix: boolean = msg.content.startsWith(prefix);
+        const startsWithMention: boolean = msg.content.startsWith(`<@${discordClient.user?.id}> `) || msg.content.startsWith(`<@!${discordClient.user?.id}> `);
+        if (msg.author.id !== discordClient.user?.id && (startsWithPrefix || startsWithMention)) {
             const tokens: string[] = msg.content.split(' ');
-            // Removes the guild-specific prefix from the command
-            tokens[0] = tokens[0].slice(prefix.length);
-            switch (tokens[0]) {
-                case 'ping':
-                    msg.reply('pong!');
-                    break;
-                default:
-                    msg.reply('invalid command!');
+            if (startsWithPrefix) {
+                // Removes the guild-specific prefix from the command
+                tokens[0] = tokens[0].slice(prefix.length);
+            } else {
+                // Removes the first token (i.e., the @Bot mention)
+                tokens.shift();
             }
+            handleCommand(tokens, msg);
         }
     });
 
     discordClient.on('guildCreate', (guild: Guild) => {
-        guilds.push({ id: guild.id, prefix: DEFAULT_PREFIX, calendars: [] });
+        guilds.set(guild.id, { id: guild.id, prefix: DEFAULT_PREFIX, calendars: [] });
         saveData();
     });
 }
 
-setUpListeners();
 startUp();
+setUpListeners();
